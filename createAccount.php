fd4e940110a5ab7ddfef5b92a57d37823095c140
robertmorelli <bobbyondino@gmail.com>
2022-04-11 20:40:12 -0600

    login and create supposedly working

--- createAccount.php
+++ createAccount.php
@@ -5,12 +5,11 @@ require_once "DataBaseConnection.php";
 $rawdata = $_GET["data"];
 $jsondata = base64_decode($rawdata);
 $data = json_decode($jsondata, true);
-
 $username = base64_encode($data['Name']);
 $password = $data['Password'];
 $avatar = $data['Avatar'];
 
-$sql = "SELECT * FROM `Users` where Name=\"$usename\";";
+$sql = "SELECT * FROM `Users` where Name=\"$username\";";
 $result = $con->query($sql);
 if (!$result) {
     $message = "Whole query " . $sql;
--- login.php
+++ login.php
@@ -5,18 +5,16 @@ $rawdata = $_GET["data"];
 $jsondata = base64_decode($rawdata);
 $data = json_decode($jsondata, true);
 
+
 $username = base64_encode($data['Name']);
 $password = $data['Password'];
 
-echo $username;
 
 $Hashed = hash("ripemd128", $password);
-$sql = "SELECT * FROM `Users` where Name=\"$usename\" AND Password=\"$Hashed\";";
+$sql = "SELECT * FROM `Users` where Name=\"$username\" AND Password=\"$Hashed\";";
 $result = $con->query($sql);
 if (!$result) {
-    $message = "Whole query " . $sql;
-    echo $message;
-    die("Invalid query: " . mysqli_error($con));
+    echo "false";
 } else {
     $count = $result->num_rows;
     if($count==1){
@@ -24,6 +22,6 @@ if (!$result) {
     }
     else{
         $message = "Whole query " . $sql;
-        echo $message;
+        echo "false";
     }
 }
